ea796cb57d8b91e43fdc000d79940066d5d583cd
jws7 <jamie1988@gmail.com>
2016-06-19 22:52:40 +0100

    Following tutorial on creating  Media Player app

--- app/src/main/java/net/jws7/player/MyActivity.java
+++ app/src/main/java/net/jws7/player/MyActivity.java
@@ -1,5 +1,6 @@
 package net.jws7.player;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,41 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MyActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MyActivity extends AppCompatActivity{
+
+    private ImageButton btnPlay;
+    private ImageButton btnForward;
+    private ImageButton btnBackward;
+    private ImageButton btnNext;
+    private ImageButton btnPrevious;
+    private ImageButton btnPlaylist;
+    private ImageButton btnRepeat;
+    private ImageButton btnShuffle;
+    private SeekBar songProgressBar;
+    private TextView songTitleLabel;
+    private TextView songCurrentDurationLabel;
+    private TextView songTotalDurationLabel;
+
+    // Media Player
+    private MediaPlayer mp;
+
+
+    private SongsManager songManager;
+
+    private int seekForwardTime = 5000; // 5000 milliseconds
+    private int seekBackwardTime = 5000; // 5000 milliseconds
+    private int currentSongIndex = 0;
+    private boolean isShuffle = false;
+    private boolean isRepeat = false;
+    private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,4 +83,6 @@ public class MyActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
